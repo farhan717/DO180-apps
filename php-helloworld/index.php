eef1af1f3324d477ac65e39a2b9b317121044267
Student User <student@workstation.lab.example.com>
2023-01-17 00:47:28 -0500

    with photo.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -4,5 +4,5 @@ print "Today is " . date("Y/m/d") . "<br>";
 print "Today is " . date("Y.m.d") . "<br>";
 print "Today is " . date("Y-m-d") . "<br>";
 print "Today is " . date("l");
-<img src="coffee.jpg" />
+
 ?>
