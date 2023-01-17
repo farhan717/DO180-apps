dad89df799c337190e7e80b6092e36cbed3e9053
Student User <student@workstation.lab.example.com>
2023-01-17 00:26:41 -0500

    Changed index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,7 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Today is " . date("Y/m/d") . "<br>";
+print "Today is " . date("Y.m.d") . "<br>";
+print "Today is " . date("Y-m-d") . "<br>";
+print "Today is " . date("l");
 ?>
